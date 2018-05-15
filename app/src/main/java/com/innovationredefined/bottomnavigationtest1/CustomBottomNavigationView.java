b69b0cb960c0f1db3c6fd12615c882f9b075c326
Arpan Bag <arpanbag1996@users.noreply.github.com>
2018-05-15 12:14:30 +0530

    Using custom BottomNavigationView

---
+++ app/src/main/java/com/innovationredefined/bottomnavigationtest1/CustomBottomNavigationView.java
@@ -0,0 +1,11 @@
+package com.innovationredefined.bottomnavigationtest1;
+
+import android.content.Context;
+import android.support.design.widget.BottomNavigationView;
+import android.util.AttributeSet;
+
+public class CustomBottomNavigationView extends BottomNavigationView {
+    public CustomBottomNavigationView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
